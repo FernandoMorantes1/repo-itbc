4f271787ccdb6772fe2f3df3a78fa333ebd47ca5
FernandoMorantes1 <fernando.morantes@mercadolibre.com.co>
2021-12-16 16:06:57 -0500

    Primer ejercicio JS

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("TEST");
+function calculatePrice(itemName, itemPrice) {
+  if (isNaN(itemPrice) || itemPrice < 1) {
+    return "El precio ingresado no es valido";
+  } else {
+    let shippingPrice;
+    let finalPrice;
+
+    if (itemPrice > 0 && itemPrice <= 2000) shippingPrice = 300;
+    else if (itemPrice > 2000 && itemPrice <= 4000) shippingPrice = 500;
+    else shippingPrice = 700;
+
+    finalPrice = itemPrice + shippingPrice;
+
+    return `El producto ${itemName} cuesta ${itemPrice}. Su costo de envio es de ${shippingPrice}. Por lo tanto, el precio final es de ${finalPrice}`;
+  }
+}
+
+console.log(calculatePrice("Macbook", 2500));
+console.log(calculatePrice("Celular", 500));
+console.log(calculatePrice("Playstation", 4500));
